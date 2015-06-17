3446ddef3b42a9089ff5c3f35e73283af95eb1b3
kennethsabir <kensab@garvan.org.au>
2015-06-17 12:06:17 +1000

    fix finding the root path

--- common/config.js
+++ common/config.js
@@ -1,19 +1,21 @@
 var fs = require('fs');
 var path = require('path');
 
-var configFile = __dirname + '/../../site.json';
+var appDir = path.dirname(require.main.filename);
+
+var configFile = appDir + '/site.json';
 var configJSON = fs.readFileSync(configFile);
 var config = JSON.parse(configJSON);
 
 
 
-config['BasePath'] = path.normalize(__dirname + "/../..");
+config['BasePath'] = appDir;
 config['ApplicationName'] = path.basename(config['BasePath']);
 
 module.exports.get = function(key) {
-	return config[key];
+    return config[key];
 }
 
 module.exports.set = function(key, value) {
-	config[key] = value;
+    config[key] = value;
 }
